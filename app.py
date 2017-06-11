525f309f3ace99fe05980faa3531aefd1d5bef56
xrodneylee <xrodneylee@gmail.com>
2017-06-11 23:18:36 +0800

    init_html

--- app.py
+++ app.py
@@ -5,12 +5,24 @@ import os
 import yaml
 app = Flask(__name__)
 
+templates_path = 'templates/'
+
 @app.before_first_request
 def init_html():
-    file = open('qq.html', 'a', encoding='UTF-8')
-    file.write('test')
+    stream = open('template.yaml', 'r')
+    data = yaml.safe_load(stream)
+    for service in data:
+      html_str = '{% extends "index.html" %}\n'
+      html_str += '{% block content %}\n'
+      html_str += '<h1>' + service + '</h1>'
+      html_str += '{% endblock content %}'
+      file = open(templates_path + service + '.html', 'w', encoding='UTF-8')
+      file.write(html_str)
+      app.logger.info(service)
+      app.logger.info(data[service])
+      app.logger.info(html_str)
+
     file.close()
-    app.logger.info('Hello world again!')
 
 @app.route('/')
 def index():
@@ -35,14 +47,6 @@ def ssh():
       os.remove('id_rsa.pub')
       
     return render_template('ssh.html', private_key=private_key, public_key=public_key)
-
-@app.route('/gunpg')
-def gunpg():
-    html = 'index'
-    stream = open('config.yaml', 'r')
-    data = yaml.load(stream)
-    app.logger.info(data)
-    return render_template('ssh.html', private_key=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
