99864962599c09372983d876f4820f774eaf3f7d
Lee Guan Pu <xrodneylee@infinitiessoft.com>
2017-06-12 11:52:26 +0800

    init html, select option tag, config.yaml

--- app.py
+++ app.py
@@ -1,28 +1,51 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-import logging
 from logging.handlers import RotatingFileHandler
+import logging
 import os
 import yaml
-app = Flask(__name__)
 
+app = Flask(__name__)
 templates_path = 'templates/'
+config_file = 'template.yaml'
 
 @app.before_first_request
 def init_html():
-    stream = open('template.yaml', 'r')
+    stream = open(config_file, 'r')
     data = yaml.safe_load(stream)
     for service in data:
+      app.logger.info('init html=' + service)
       html_str = '{% extends "index.html" %}\n'
       html_str += '{% block content %}\n'
       html_str += '<h1>' + service + '</h1>\n'
+      html_str += '<div class="console">\n'
+      html_str += '<form class="pure-form pure-form-aligned" action="/' + service + '" method="post">\n'
+      html_str += '<fieldset>\n'
+      html_str += '<legend>Input</legend>\n'
+
+      for element in data[service]:
+        html_str += '<div class="pure-control-group">\n'
+        html_str += '<label>' + element + '</label>'
+        html_str += '<span>' + element + '</span>'
+        # if element == 'description':
+        #   html_str += ''
+
+
+      html_str += '</fieldset>\n'
+      html_str += '</form>\n'
+      html_str += '<form class="pure-form pure-form-aligned">'
+      html_str += '<fieldset>\n'
+      html_str += '<legend>Output</legend>\n'
+
+
+
+      html_str += '</fieldset>\n'
+      html_str += '</form>\n'
+      html_str += '</div>\n'
       html_str += '{% endblock content %}'
       file = open(templates_path + service + '.html', 'w', encoding='UTF-8')
       file.write(html_str)
-      # app.logger.info(service)
-      # app.logger.info(data[service])
-      # app.logger.info(html_str)
 
     file.close()
 
@@ -61,7 +84,7 @@ def index(service=None):
 #     return render_template('gunpg.html')
 
 if __name__ == '__main__':
-    handler = RotatingFileHandler('foo.log', maxBytes=10000, backupCount=1)
+    handler = RotatingFileHandler('app.log', maxBytes=10000, backupCount=1)
     handler.setLevel(logging.INFO)
     app.logger.addHandler(handler)
     app.run(debug=True)
